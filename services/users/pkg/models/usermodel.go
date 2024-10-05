e5b60f81ce1d7ed96e3953ed589d6531d3acfa40
Rushin Shah <shahrushin32@gmail.com>
2024-10-05 18:58:42 +0530

    improve user model.

--- services/users/pkg/models/usermodel.go
+++ services/users/pkg/models/usermodel.go
@@ -5,7 +5,11 @@ import (
 )
 
 type User struct {
-	ID    primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	Name  string             `bson:"name,omitempty" json:"name"`
-	Email string             `bson:"email,omitempty" json:"email"`
+	ID              primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Name            string             `bson:"name,omitempty" json:"name"`
+	Email           string             `bson:"email,omitempty" json:"email"`
+	Role            string             `bson:"role,omitempty" json:"role,omitempty"`
+	Password        string             `bson:"password,omitempty" json:"password,omitempty"`
+	ConfirmPassword string             `bson:"confirmPassword,omitempty" json:"confirmPassword,omitempty"`
+	CreatedAt       primitive.DateTime `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
 }
