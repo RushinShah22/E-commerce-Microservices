8194ff2a9937a374640442bc16b33295b9d903d1
Rushin Shah <shahrushin32@gmail.com>
2024-10-08 04:42:04 +0530

    change user model.

--- services/users/pkg/models/usermodel.go
+++ services/users/pkg/models/usermodel.go
@@ -5,12 +5,11 @@ import (
 )
 
 type User struct {
-	ID              primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	FirstName       string             `bson:"firstName,omitempty" json:"firstName"`
-	LastName        string             `bson:"lastName,omitempty" json:"lastName,omitempty"`
-	Email           string             `bson:"email,omitempty" json:"email"`
-	Role            string             `bson:"role,omitempty" json:"role,omitempty"`
-	Password        string             `bson:"password,omitempty" json:"password,omitempty"`
-	ConfirmPassword string             `bson:"confirmPassword,omitempty" json:"confirmPassword,omitempty"`
-	CreatedAt       primitive.DateTime `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	FirstName string             `bson:"firstName,omitempty" json:"firstName"`
+	LastName  string             `bson:"lastName,omitempty" json:"lastName,omitempty"`
+	Email     string             `bson:"email,omitempty" json:"email"`
+	Role      string             `bson:"role,omitempty" json:"role,omitempty"`
+	Password  string             `bson:"password,omitempty" json:"password,omitempty"`
+	CreatedAt primitive.DateTime `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
 }
