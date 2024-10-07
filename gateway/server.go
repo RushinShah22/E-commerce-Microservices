4199f846e83970d3960a6a0f829be96b9bb184af
Rushin Shah <shahrushin32@gmail.com>
2024-10-08 04:39:58 +0530

    init implementation of graphql schema.

---
+++ gateway/server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/RushinShah22/e-commerce-micro/gateway/graph"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		UserURL:    "users/users",
+		ProductURL: "products/products",
+		OrderURL:   "orders/orders",
+	}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
